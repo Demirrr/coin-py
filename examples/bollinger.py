b26f7bd03f06dc84f4411e0d331f283350621fb7
Caglar Demir <caglardemir8@gmail.com>
2021-05-08 09:39:13 +0200

    Bollinger bands with different std.

--- examples/bollinger.py
+++ examples/bollinger.py
@@ -5,6 +5,8 @@ dfs = cp.DataFramesHolder(path='../Data')
 dfs.drop_data_frames(key=lambda x: len(x) < 1000)
 dfs.preprocess({'func': 'mean', 'input': ['open', 'close'], 'output': 'price'})
 dfs.select_col(['price'])
-dfs.select_interval(start="2021-03-25", end="2021-04-20")
-dfs.bollinger_bands('BTC', window_size=12 * 12)  # each window 5 minutes
-dfs.plot(coin='BTC')
+dfs.select_interval(start="2021-03-25")
+# or dfs.select_interval(start="2021-03-25", end="2021-04-20")
+for i in [1, 2, 3]:
+    dfs.bollinger_bands('BTC', standard_variation=i, window_size=12 * 12)  # each window 5 minutes
+    dfs.plot(coin='BTC')
