ac30cf9099b0330241af528cd12fc64d7de9ead1
Caglar Demir <caglardemir8@gmail.com>
2021-05-08 09:47:48 +0200

    printout corrected.

--- examples/correlation.py
+++ examples/correlation.py
@@ -1,20 +1,24 @@
 import coinpy as cp
+
 dfs = cp.DataFramesHolder(path='../Data')
 dfs.drop_data_frames(key=lambda x: len(x) < 1000)
 dfs.preprocess({'func': 'mean', 'input': ['open', 'close'], 'output': 'price'})
 dfs.sort_frames()
 dfs.select_col(['price'])
 dfs.dropna()
+
 dfs.corr(x='XLM', y='UNI')
 dfs.corr(x='XLM', y='ETH')
+dfs.corr(x='XLM', y='BTC')
 dfs.corr(x='ETH', y='UNI')
 dfs.corr(x='BTC', y='UNI')
 dfs.corr(x='BTC', y='ETH')
 
 corr = dfs.corr()
-# Top 10 positive correlation
-for (c, info) in corr[:10]:
-    print(info, c)
-# Top 10 negative correlation
-for (c, info) in corr[-10:]:
-    print(info, c)
+print('\n\nTop 10 positive correlation')
+for i, (c, info) in enumerate(corr[:10]):
+    print(f'{i+1}. {info}')
+
+print('\n\nTop 10 negative correlation')
+for i, (c, info) in enumerate(corr[-10:]):
+    print(f'{i+1}. {info}')
